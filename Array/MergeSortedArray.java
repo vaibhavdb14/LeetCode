61c43b588b93fa2f56980bcedfa1c281d1f2664d
Vaibhav Bodade <vdbodade140705@gmail.com>
2025-11-19 08:46:13 +0530

    Solved Merge Two Sorted Array Problem

---
+++ Array/MergeSortedArray.java
@@ -0,0 +1,58 @@
+/*
+ * LeetCode Problem 88: Merge Sorted Array (easy)
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * 
+ * Example 1:
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ * 
+ * Credits: I will implement the two-pointer technique to efficiently merge the two sorted arrays in-place.)
+*/
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        // My initial approach (less optimal)
+        // int i = 0;
+        // for(int j = m; j<(m+n); j++){
+        //     nums1[j] = nums2[i];
+        //     i++;
+        // }
+        // Arrays.sort(nums1);
+
+        // My Second approach (optimal)
+        int n1 = m - 1;
+        int n2 = n - 1;
+        int i = m + n - 1;
+
+        while (n2 >= 0) {   
+            if (n1 >= 0 && nums1[n1] > nums2[n2]) {
+                nums1[i] = nums1[n1];
+                n1--;
+            } else {
+                nums1[i] = nums2[n2];
+                n2--;
+            }
+            i--;
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArray msa = new MergeSortedArray();
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+        msa.merge(nums1, m, nums2, n);
+
+        System.out.print("Merged array: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
